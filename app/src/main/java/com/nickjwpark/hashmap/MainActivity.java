a7315b74aa10f7e784ebe13e7ef06b6e397208e6
Nick JongWook Park <Nick@Nicks-MacBook-Pro.local>
2016-06-04 16:29:41 +0900

    Fixed errors

--- app/src/main/java/com/nickjwpark/hashmap/MainActivity.java
+++ app/src/main/java/com/nickjwpark/hashmap/MainActivity.java
@@ -13,7 +13,7 @@ public class MainActivity extends AppCompatActivity {
 
     Button btnAdd;
     EditText editTextName, editTextHeight;
-    HashMap height;
+    HashMap <String, Double> height;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,13 +24,13 @@ public class MainActivity extends AppCompatActivity {
         editTextName =(EditText) findViewById(R.id.editTextName);
         editTextHeight = (EditText) findViewById(R.id.editTextHeight);
 
-        height = new HashMap();
+        height = new HashMap<String, Double>();
 
         btnAdd.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
                 String name = editTextName.getText().toString();
-                double height_val = Double.parseDouble(editTextHeight.getText().toString());
+                Double height_val = Double.parseDouble(editTextHeight.getText().toString());
                 height.put(name, height_val);
                 Log.d("debugging", name + "의 키는 " + height_val + "cm 입니다");
             }
