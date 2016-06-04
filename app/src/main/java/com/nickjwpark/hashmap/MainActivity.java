3e1ed80cc6661209bd896814339c2552d81f6d64
Nick JongWook Park <Nick@Nicks-MacBook-Pro.local>
2016-06-04 16:24:06 +0900

    Initial commit

---
+++ app/src/main/java/com/nickjwpark/hashmap/MainActivity.java
@@ -0,0 +1,39 @@
+package com.nickjwpark.hashmap;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import java.util.HashMap;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btnAdd;
+    EditText editTextName, editTextHeight;
+    HashMap height;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnAdd = (Button) findViewById(R.id.btnAdd);
+        editTextName =(EditText) findViewById(R.id.editTextName);
+        editTextHeight = (EditText) findViewById(R.id.editTextHeight);
+
+        height = new HashMap();
+
+        btnAdd.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                String name = editTextName.getText().toString();
+                double height_val = Double.parseDouble(editTextHeight.getText().toString());
+                height.put(name, height_val);
+                Log.d("debugging", name + "의 키는 " + height_val + "cm 입니다");
+            }
+        });
+    }
+}
