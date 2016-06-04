b59da83bce739c7b0d25c5aa4b018add9b21c738
Nick JongWook Park <Nick@Nicks-MacBook-Pro.local>
2016-06-04 16:50:42 +0900

    Hashmap of Array

--- app/src/main/java/com/nickjwpark/hashmap/MainActivity.java
+++ app/src/main/java/com/nickjwpark/hashmap/MainActivity.java
@@ -12,8 +12,8 @@ import java.util.HashMap;
 public class MainActivity extends AppCompatActivity {
 
     Button btnAdd;
-    EditText editTextName, editTextHeight;
-    HashMap <String, Double> height;
+    EditText editTextName, editTextHeight, editTextWeight, editTextAge;
+    HashMap <String, double[]> height;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,17 +23,26 @@ public class MainActivity extends AppCompatActivity {
         btnAdd = (Button) findViewById(R.id.btnAdd);
         editTextName =(EditText) findViewById(R.id.editTextName);
         editTextHeight = (EditText) findViewById(R.id.editTextHeight);
+        editTextWeight = (EditText) findViewById(R.id.editTextWeight);
+        editTextAge = (EditText) findViewById(R.id.editTextAge);
 
-        height = new HashMap<String, Double>();
+        height = new HashMap<String, double[]>();
 
         btnAdd.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
                 String name = editTextName.getText().toString();
                 Double height_val = Double.parseDouble(editTextHeight.getText().toString());
-                height.put(name, height_val);
+                Double weight_val = Double.parseDouble(editTextWeight.getText().toString());
+                //나이는 원래 정수이지만 double 배열을 쓰기 때문에 double 로 사용해 주도록 하자
+                Double age_val = Double.parseDouble(editTextAge.getText().toString());
+
+                double [] values = {height_val, weight_val, age_val};
+                height.put(name, values);
                 for (String key : height.keySet()) {
-                    Log.d("debugging", key + "의 키는 " + height.get(key) + "cm 입니다");
+                    Log.d("debugging", key + "의 키는 " + height.get(key)[0] + "cm,"+
+                            " 몸무게는 " + height.get(key)[1] + "kg,"+
+                            " 나이는 " + height.get(key)[2] + "살 입니다");
                 }
             }
         });
